09dd8085a571d544ad5b23a5d1c5482af8d8285b
wilber-c-o <wilbercamposortiz@gmail.com>
2026-09-08 00:16:15 +0200

    Agregar rutas para Tutores e Historial

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -10,8 +10,8 @@ import LoginPage from "../pages/auth/LoginPage";
 import AsientosPage from "../pages/AsientosPage";
 import EstudiantesPage from "../pages/EstudiantesPage";
 import CursosPage from "../pages/CursosPage";
-import AsignacionesPage from "../pages/AsignacionesPage";
-import ConfiguracionPage from "../pages/ConfiguracionPage";
+import TutoresPage from "../pages/TutoresPage";
+import HistorialPage from "../pages/HistorialPage";
 import { authRepository } from "../repositories/authRepository";
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
@@ -30,10 +30,10 @@ function AppRoutes() {
         <Route path="/asientos" element={<ProtectedRoute><AsientosPage /></ProtectedRoute>} />
         <Route path="/estudiantes" element={<ProtectedRoute><EstudiantesPage /></ProtectedRoute>} />
         <Route path="/cursos" element={<ProtectedRoute><CursosPage /></ProtectedRoute>} />
+        <Route path="/tutores" element={<ProtectedRoute><TutoresPage /></ProtectedRoute>} />
+        <Route path="/historial" element={<ProtectedRoute><HistorialPage /></ProtectedRoute>} />
         {/* Compatibilidad con el enlace anterior */}
         <Route path="/clases" element={<Navigate to="/cursos" replace />} />
-        <Route path="/asignaciones" element={<ProtectedRoute><AsignacionesPage /></ProtectedRoute>} />
-        <Route path="/configuracion" element={<ProtectedRoute><ConfiguracionPage /></ProtectedRoute>} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
