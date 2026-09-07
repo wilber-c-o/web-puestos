27676a7f454d7a95f458d564028fb6dce631bb5e
wilber-c-o <wilbercamposortiz@gmail.com>
2026-09-08 00:15:59 +0200

    Agregar página de Tutores

---
+++ src/pages/TutoresPage.tsx
@@ -0,0 +1,10 @@
+function TutoresPage() {
+  return (
+    <main className="page-content">
+      <h1>Tutores</h1>
+      <p>Gestión de tutores del colegio.</p>
+    </main>
+  );
+}
+
+export default TutoresPage;
