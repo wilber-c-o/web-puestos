55c6c1887eb40380874bea57fb4cf49253103716
wilber-c-o <wilbercamposortiz@gmail.com>
2026-09-07 23:30:58 +0200

    feat: cambiar ruta de Clases a Cursos

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -9,23 +9,15 @@ import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
 import AsientosPage from "../pages/AsientosPage";
 import EstudiantesPage from "../pages/EstudiantesPage";
-import ClasesPage from "../pages/ClasesPage";
+import CursosPage from "../pages/CursosPage";
 import AsignacionesPage from "../pages/AsignacionesPage";
 import ConfiguracionPage from "../pages/ConfiguracionPage";
-
 import { authRepository } from "../repositories/authRepository";
 
-function ProtectedRoute({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const isAuthenticated = authRepository.isAuthenticated();
 
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
-  }
-
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
   return children;
 }
 
@@ -33,76 +25,16 @@ function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* Página principal */}
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <HomePage />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Login */}
+        <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
         <Route path="/login" element={<LoginPage />} />
-
-        {/* Asientos */}
-        <Route
-          path="/asientos"
-          element={
-            <ProtectedRoute>
-              <AsientosPage />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Estudiantes */}
-        <Route
-          path="/estudiantes"
-          element={
-            <ProtectedRoute>
-              <EstudiantesPage />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Clases */}
-        <Route
-          path="/clases"
-          element={
-            <ProtectedRoute>
-              <ClasesPage />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Asignaciones */}
-        <Route
-          path="/asignaciones"
-          element={
-            <ProtectedRoute>
-              <AsignacionesPage />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Configuración */}
-        <Route
-          path="/configuracion"
-          element={
-            <ProtectedRoute>
-              <ConfiguracionPage />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Cualquier ruta desconocida vuelve al inicio */}
-        <Route
-          path="*"
-          element={<Navigate to="/" replace />}
-        />
-
+        <Route path="/asientos" element={<ProtectedRoute><AsientosPage /></ProtectedRoute>} />
+        <Route path="/estudiantes" element={<ProtectedRoute><EstudiantesPage /></ProtectedRoute>} />
+        <Route path="/cursos" element={<ProtectedRoute><CursosPage /></ProtectedRoute>} />
+        {/* Compatibilidad con el enlace anterior */}
+        <Route path="/clases" element={<Navigate to="/cursos" replace />} />
+        <Route path="/asignaciones" element={<ProtectedRoute><AsignacionesPage /></ProtectedRoute>} />
+        <Route path="/configuracion" element={<ProtectedRoute><ConfiguracionPage /></ProtectedRoute>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
